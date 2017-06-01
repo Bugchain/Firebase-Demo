94f28fb0eeedb9f46f665e3db0e125a1eea2aa75
R-ARP-IMD-101\androiddev <chain.chaiyaphat@gmail.com>
2017-06-01 17:29:14 +0700

    Frist create login activity.

---
+++ FirebaseDemo/app/src/main/java/com/bugchain/firebasedemo/activity/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.bugchain.firebasedemo.activity;
+/*
+ * Created by BUG CHAIN on 01/06/2017.
+ * ARIP Public Company Limited
+ * Bangkok, Thailand
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bugchain.firebasedemo.R;
+
+public class RegisterActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
